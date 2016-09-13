d9b8c3c47f08379c1f073cf3e6475a424af10a12
Shrey Sanghavi <sasanghavi@gmail.com>
2016-09-12 21:25:51 -0400

    Uploading main js file with non-commented code

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var needle = require("needle");
 var os   = require("os");
 
 var config = {};
-config.token = "c0576da34d0b5ed8e77e75b8a3fc14a05657120c4e6acb3c2f6e9e6dc32dfd57";
+config.token = "e7913305cc6a38cdb7c468848966005f4bf68dee15cc515b7673963b006f15cb";
 
 var headers =
 {
@@ -19,29 +19,50 @@ var client =
 	{
 		needle.get("https://api.digitalocean.com/v2/regions", {headers:headers}, onResponse)
 	},
-
-	createDroplet: function (dropletName, region, imageName, onResponse)
+	listImages: function( onResponse )
 	{
-		var data = 
+		needle.get("https://api.digitalocean.com/v2/images", {headers:headers}, onResponse)
+	},
+
+
+	listSshDetails: function( onResponse )
+	{
+		needle.get("https://api.digitalocean.com/v2/account/keys", {headers:headers}, onResponse)
+	},
+
+		createDroplet: function (dropletName, region, imageName, onResponse)
 		{
-			"name": dropletName,
-			"region":region,
-			"size":"512mb",
-			"image":imageName,
-			// Id to ssh_key already associated with account.
-			"ssh_keys":[625870],
-			//"ssh_keys":null,
-			"backups":false,
-			"ipv6":false,
-			"user_data":null,
-			"private_networking":null
-		};
+			var data =
+			{
+				"name": dropletName,
+				"region":region,
+				"size":"512mb",
+				"image":imageName,
+				// Id to ssh_key already associated with account.
+				"ssh_keys":[3409267],
+				//"ssh_keys":null,
+				"backups":false,
+				"ipv6":false,
+				"user_data":null,
+				"private_networking":null,
+				"volumes":null
+			};
 
-		console.log("Attempting to create: "+ JSON.stringify(data) );
+			// console.log("Attempting to create: "+ JSON.stringify(data) );
 
-		needle.post("https://api.digitalocean.com/v2/droplets", data, {headers:headers,json:true}, onResponse );
+			needle.post("https://api.digitalocean.com/v2/droplets", data, {headers:headers,json:true}, onResponse );
+		},
+	retrieve: function(dropletId, onResponse )
+	{
+		needle.get("https://api.digitalocean.com/v2/droplets/"+dropletId, {headers:headers}, onResponse);
+	}
+	};
+	delete: function(dropletId, onResponse )
+	{
+		needle.delete("https://api.digitalocean.com/v2/droplets/"+dropletId, null, {headers:headers}, onResponse);
 	}
 };
+
 
 // #############################################
 // #1 Print out a list of available regions
@@ -62,7 +83,7 @@ client.listRegions(function(error, response)
 	{
 		for(var i=0; i<data.regions.length; i++)
 		{
-
+			console.log(data.regions[i].slug)
 		}
 	}
 });
@@ -74,23 +95,66 @@ client.listRegions(function(error, response)
 // https://developers.digitalocean.com/documentation/v2/#images
 // - Print out a list of available system images, that are AVAILABLE in a specified region.
 // - use 'slug' property
+client.listImages(function(error, response)
+{
+	var data = response.body;
+	//console.log( JSON.stringify(response.body) );
+
+	if( response.headers )
+	{
+		console.log( "Calls remaining", response.headers["ratelimit-remaining"] );
+		console.log("Status code: ", response.headers["status"]);
+	}
+
+	if( data.images )
+	{
+		for(var i=0; i<data.images.length; i++)
+		{
+			console.log("Available image: \n", data.images[i].slug)
+		}
+	}
+});
+
+client.listSshDetails(function(error, response)
+{
+	var data = response.body;
+	console.log( JSON.stringify(response.body) );
+
+	if( response.headers )
+	{
+		console.log( "Calls remaining", response.headers["ratelimit-remaining"] );
+		console.log("Status code: ", response.headers["status"]);
+	}
+
+	if( data.ssh_keys )
+	{
+		for(var i=0; i<data.ssh_keys.length; i++)
+		{
+			console.log("Available key: \n", data.ssh_keys[i].id)
+		}
+	}
+});
+
 
 // #############################################
 // #3 Create an droplet with the specified name, region, and image
 // Comment out when completed. ONLY RUN ONCE!!!!!
 // Write down/copy droplet id.
-// var name = "UnityId"+os.hostname();
-// var region = ""; // Fill one in from #1
-// var image = ""; // Fill one in from #2
-// client.createDroplet(name, region, image, function(err, resp, body)
-// {
-// 	console.log(body);
-// 	// StatusCode 202 - Means server accepted request.
-// 	if(!err && resp.statusCode == 202)
-// 	{
-// 		console.log( JSON.stringify( body, null, 3 ) );
-// 	}
-// });
+var name = "ssangha"+os.hostname();
+var region = "sgp1"; // Fill one in from #1
+var image = "debian-7-x32"; // Fill one in from #2
+client.createDroplet(name, region, image, function(error, response, body)
+{
+	//console.log(body);
+	console.log( "Calls remaining", response.headers["ratelimit-remaining"] );
+	console.log("Status code: ", response.headers["status"]);
+
+	// StatusCode 202 - Means server accepted request.
+	if(!error && response.statusCode == 202)
+	{
+		console.log( JSON.stringify( body, null, 5 ) );
+	}
+});
 
 // #############################################
 // #4 Extend the client to retrieve information about a specified droplet.
@@ -98,11 +162,25 @@ client.listRegions(function(error, response)
 // https://developers.digitalocean.com/documentation/v2/#retrieve-an-existing-droplet-by-id
 // REMEMBER POST != GET
 // Most importantly, print out IP address!
-var dropletId = "3788359";
+var dropletId = "25571465";
+client.retrieve(dropletId, function(error, response)
+{
+	var data = response.body;
+	if( response.headers )
+	{
+		console.log( "Calls remaining", response.headers["ratelimit-remaining"] );
+		console.log("Status code: ", response.headers["status"]);
+	}
+
+	if( data.droplet )
+	{
+			console.log("Available droplet ip address details: \n", data.droplet.networks.v4[0].ip_address);
+	}
+});
 
 // #############################################
 // #5 In the command line, ping your server, make sure it is alive!
-// ping xx.xx.xx.xx
+ping 188.166.223.77
 
 // #############################################
 // #6 Extend the client to DESTROY the specified droplet.
@@ -116,8 +194,21 @@ var dropletId = "3788359";
 // 	{
 //			console.log("Deleted!");
 // 	}
+client.delete(dropletId, function(error, response)
+{
+	// console.log("response : ", response);
 
+	if( response.headers )
+	{
+		console.log( "Calls remaining", response.headers["ratelimit-remaining"] );
+		console.log("Status code: ", response.headers["status"]);
+	}
+	if(!error && response.statusCode == 204)
+	{
+		console.log("Deleted!");
+	}
+});
 // #############################################
 // #7 In the command line, ping your server, make sure it is dead!
-// ping xx.xx.xx.xx
+ping 188.166.223.77
 // It could be possible that digitalocean reallocated your IP address to another server, so don't fret it is still pinging.
